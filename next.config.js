6f3d8f62e7da6f676ae9495bf7af071a6851ad46
Aleksandr Bukhalo <a@bukhalo.com>
2020-03-07 12:34:58 +0300

    Fix Dockerfile (#5)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    STREAMING_HELL_API_ENDPOINT: process.env.STREAMING_HELL_API_ENDPOINT,
+    STREAMING_HELL_API_ENDPOINT: process.env.STREAMING_HELL_API_ENDPOINT || 'http://95.216.141.51:3000/api/v1-alpha.1',
   },
   webpack(config) {
     config.module.rules.push({
