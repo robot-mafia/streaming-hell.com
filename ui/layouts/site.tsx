8c9093341f63e71b00e01125bd89c5be76bd68f3
Aleksandr Bukhalo <a@bukhalo.com>
2020-04-08 13:59:33 +0300

    feat: markup footer with stores icons
    #9

--- ui/layouts/site.tsx
+++ ui/layouts/site.tsx
@@ -1,10 +1,18 @@
+import styled from 'styled-components';
 import { Navigation } from '../navigation';
+import { Footer } from '../footer';
+
+const Layout = styled.div`
+  height: 100%;
+  min-height: 100%;
+`;
 
 export const SiteLayout: React.FC = ({ children }) => {
   return (
-    <div>
+    <Layout>
       <Navigation />
       {children}
-    </div>
+      <Footer />
+    </Layout>
   );
 };
