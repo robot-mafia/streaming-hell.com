9efa21fdd3364b5e2557d9e17b07234a452584e4
Alexander Bukhalo <alex@bukhalo.com>
2022-04-18 15:53:48 +0300

    try to fix dockerfile

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
+    // pnpm virtual-store-dir is outside the app directory
+    outputFileTracingRoot: path.resolve(__dirname, '../'),
   },
 };
 
